f0c7fefea1da4c5896d8ef0eef8a258a93f94c65
Shahzar <shazerali27@gmail.com>
2026-05-12 14:26:52 +0500

    fixed subcategories

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,17 +1,17 @@
 export const categories = [
     "all",
     "clothes",
-    "mens clothes",
-    "kids clothes",
     "shoes",
     "watches",
     "fashion bags",
+    "mens clothes",
+    "kids clothes",
     "kitchenware",
     "home decor",
     "Electronic Accessories",
 ];
 
-export const subcategories = {
+export const categoryFilters = {
     clothes: ["shirt", "2 Piece suits", "3 Piece suits", "kurti", "stiched", "lehnega"],
     "mens clothes": ["cotton", "silk", "karandi", "wash n wear", "wool", "boski", "khaddar", "mens orginal", "stiched", "shirts"],
     "kids clothes": ["full dress", "girls kurta", "kids hoodies", "west", "bottoms", "kids formal wear"],
@@ -25,22 +25,3 @@ export const subcategories = {
         "analog",
         "luxury",]
 };
-
-export const colorOptions = [
-    "Black",
-    "Blue",
-    "Pink",
-    "White",
-    "Red",
-    "Purple",
-    "Brown",
-    "Green",
-    "Yellow",
-];
-
-export const sizeOptions = [
-
-    "6", "7", "8", "9", "10",
-    "35", "36", "37", "38", "39", "40",
-    "1-2 years", "3-4 years", "5-6 years", "7-8 years", "9-10 years", "11-12 years",
-];
